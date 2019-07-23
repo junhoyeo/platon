81697d0b41d848ce625d35c3e57d212d756f306d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-24 01:46:23 +0900

    test(lexer): refactor test names and description

--- test/lexer/test_numbers.py
+++ test/lexer/test_numbers.py
@@ -4,33 +4,46 @@ import unittest
 
 class TokenizeNumber(unittest.TestCase):
 
-    def test_number(self):
-        '''한 자리 정수 토크나이징'''
-        self.assertEqual(lexResult('0')[0], (TokenType.NUMBER, 0))
-        self.assertEqual(lexResult('1')[0], (TokenType.NUMBER, 1))
+    def test_numbers(self):
+        '''tokenizing single-digit numbers'''
+        self.assertEqual(lexResult('0')[0], (TokenType.NUMBER, 0),
+            'parse 0 to 0')
+        self.assertEqual(lexResult('1')[0], (TokenType.NUMBER, 1),
+            'parse 1 to 1')
 
-    def test_multidigit_number(self):
-        '''여러 자리 정수 토크나이징'''
-        self.assertEqual(lexResult('10')[0], (TokenType.NUMBER, 10))
-        self.assertEqual(lexResult('120')[0], (TokenType.NUMBER, 120))
-        self.assertEqual(lexResult('99999')[0], (TokenType.NUMBER, 99999))
+    def test_multidigit_numbers(self):
+        '''tokenizing of multi-digit numbers'''
+        self.assertEqual(lexResult('10')[0], (TokenType.NUMBER, 10),
+            'parse 10 to 10')
+        self.assertEqual(lexResult('120')[0], (TokenType.NUMBER, 120),
+            'parse 120 to 120')
+        self.assertEqual(lexResult('99999')[0], (TokenType.NUMBER, 99999),
+            'parse 99999 to 99999')
     
-    def test_float_number(self):
-        '''실수 파싱'''
-        self.assertEqual(lexResult('0.5')[0], (TokenType.NUMBER, 0.5))
-        self.assertEqual(lexResult('1.2')[0], (TokenType.NUMBER, 1.2))
+    def test_float_numbers(self):
+        '''parsing floats'''
+        self.assertEqual(lexResult('0.5')[0], (TokenType.NUMBER, 0.5),
+            'parse 0.5 to 0.5')
+        self.assertEqual(lexResult('1.2')[0], (TokenType.NUMBER, 1.2),
+            'parse 1.2 to 1.2')
 
-    def test_numerals_bin(self):
-        '''2진수 파싱'''
-        self.assertEqual(lexResult('0b0')[0], (TokenType.NUMBER, 0))
-        self.assertEqual(lexResult('0b101')[0], (TokenType.NUMBER, 5))
-        self.assertEqual(lexResult('0b1001110')[0], (TokenType.NUMBER, 78))
+    def test_binary_numerals(self):
+        '''parsing binary'''
+        self.assertEqual(lexResult('0b0')[0], (TokenType.NUMBER, 0),
+            'parse 0b0 to 0')
+        self.assertEqual(lexResult('0b101')[0], (TokenType.NUMBER, 5),
+            'parse 0b101 to 5')
+        self.assertEqual(lexResult('0b1001110')[0], (TokenType.NUMBER, 78),
+            'parse 0b1001110 to 78')
 
-    def test_numerals_oct(self):
-        '''8진수 파싱'''
-        self.assertEqual(lexResult('0o0')[0], (TokenType.NUMBER, 0))
-        self.assertEqual(lexResult('0o7')[0], (TokenType.NUMBER, 7))
-        self.assertEqual(lexResult('0o704')[0], (TokenType.NUMBER, 452))
+    def test_octal_numerals(self):
+        '''parsing octal'''
+        self.assertEqual(lexResult('0o0')[0], (TokenType.NUMBER, 0),
+            'parse 0o0 to 0')
+        self.assertEqual(lexResult('0o7')[0], (TokenType.NUMBER, 7),
+            'parse 0o7 to 7')
+        self.assertEqual(lexResult('0o704')[0], (TokenType.NUMBER, 452),
+            'parse 0o704 to 452')
 
 if __name__ == '__main__':
     unittest.main()
