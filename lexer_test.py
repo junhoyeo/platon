70b89bca2a120a72bd4563143876670f8ba6ec8f
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-23 18:22:17 +0900

    test(lexer): unit tests for number tokenization

--- lexer_test.py
+++
@@ -1,8 +0,0 @@
-from platon.lexer import Lexer
-
-if __name__ == '__main__':
-    while 1:
-        code = input('> ')
-        lexer = Lexer(code)
-        tokens = lexer.lex()
-        print(*tokens, sep='\n')
